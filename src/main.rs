b4425180b809bec6cf93d869316c4497cb529a39
Hironha <rafaelhirokato@gmail.com>
2024-07-21 17:44:48 -0300

    rearrange routes to supporte dashboard

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,9 @@ use sqlx::PgPool;
 use tokio::net::TcpListener;
 
 use crate::app::catalog::api as catalog_api;
+use crate::app::catalog::dashboard as catalog_dashboard;
 use crate::app::extra::api as extra_api;
+use crate::app::extra::dashboard as extra_dashboard;
 use crate::app::product::api as product_api;
 
 #[derive(Clone, Debug)]
@@ -44,38 +46,48 @@ async fn main() {
     let context = Context { pool };
 
     let app = Router::new()
-        .route("/dashboard/assets/main.css", routing::get(serve_css))
-        .route("/health-check", routing::get(health_check))
-        .route(
-            "/catalogs",
-            routing::get(catalog_api::list).post(catalog_api::create),
+        .nest(
+            "/api",
+            Router::new()
+                .route(
+                    "/catalogs",
+                    routing::get(catalog_api::list).post(catalog_api::create),
+                )
+                .route(
+                    "/catalogs/:id",
+                    routing::delete(catalog_api::delete)
+                        .get(catalog_api::find)
+                        .put(catalog_api::update),
+                )
+                .route(
+                    "/catalogs/:catalog_id/products",
+                    routing::post(product_api::create),
+                )
+                .route(
+                    "/catalogs/:catalog_id/products/:id",
+                    routing::delete(product_api::delete)
+                        .get(product_api::find)
+                        .put(product_api::update),
+                )
+                .route(
+                    "/extras",
+                    routing::get(extra_api::all).post(extra_api::create),
+                )
+                .route(
+                    "/extras/:id",
+                    routing::delete(extra_api::delete).put(extra_api::update),
+                ),
         )
-        .route(
-            "/catalogs/:id",
-            routing::delete(catalog_api::delete)
-                .get(catalog_api::find)
-                .put(catalog_api::update),
+        .nest(
+            "/dashboard",
+            Router::new()
+                .route("/assets/main.css", routing::get(serve_css))
+                .route("/catalogs", routing::get(catalog_dashboard::list))
+                .route("/extras", routing::get(extra_dashboard::all))
+                .route("/health-check", routing::get(health_check))
+                .fallback(routing::any(not_found)),
         )
-        .route(
-            "/catalogs/:catalog_id/products",
-            routing::post(product_api::create),
-        )
-        .route(
-            "/catalogs/:catalog_id/products/:id",
-            routing::delete(product_api::delete)
-                .get(product_api::find)
-                .put(product_api::update),
-        )
-        .route(
-            "/extras",
-            routing::get(extra_api::all).post(extra_api::create),
-        )
-        .route(
-            "/extras/:id",
-            routing::delete(extra_api::delete).put(extra_api::update),
-        )
-        .with_state(context)
-        .fallback(routing::any(not_found));
+        .with_state(context);
 
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
